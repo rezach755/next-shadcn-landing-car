88328f9689d083534730040999afefbb6c5e15ee
reza <reza.ch755@gmail.com>
2025-03-10 22:11:29 +0330

    seo

--- next-sitemap.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://www.sarvestan-emdad.ir", // Replace with your domain
-  generateRobotsTxt: true, // Also generates a robots.txt file
-}
--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true, // Ensure App Router features are enabled
+  },
   images: {
-    domains: ['picsum.photos'],
+    domains: ["picsum.photos"],
   },
 }
 
